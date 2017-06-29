d642b771134afd4f5b41dc73dd6eb19055cee084
yonseung kim <ykim@yonseungs-MacBook-Pro.local>
2017-06-29 16:43:56 -0400

    Assign Colours To Each User

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,10 +3,18 @@ import React, {Component} from 'react';
 class Message extends Component {
   renderMessage() {
     if (this.props.message.type === "incomingMessage") {
+      let content = this.props.message.content;
+      const myRe = /https?:\/\/.*?\.(?:png|jpg|jpeg|gif)/ig;
+      const isFindImg = myRe.test(content);
+      if (isFindImg) {
+        content = <img className="message-img" src={content}/>
+      } 
       return (
         <div className="message">
           <span className="message-username" style={{color: this.props.message.color}}>{this.props.message.username}</span>
-          <span className="message-content">{this.props.message.content}</span>
+          <span className="message-content">
+            {content}
+          </span>
         </div>
       );
     } else if (this.props.message.type == "incomingNotification") {
